5d986b6f0d51d6426c1f5c97f248c9bea25200b1
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-07-27 00:33:03 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,14 +68,14 @@ const Navbar: React.FC = () => {
                   <span
                     className={
                       item.special
-                        ? 'bg-clip-text text-transparent bg-gradient-to-br from-purple-700 via-purple-600 to-purple-700 dark:from-yellow-400 dark:via-yellow-300 dark:to-yellow-200'
+                        ? 'bg-clip-text text-transparent bg-gradient-to-br from-purple-700 via-purple-600 to-purple-700 dark:from-yellow-600 dark:via-yellow-300 dark:to-yellow-700'
                         : ''
                     }
                   >
                     {item.label}
                   </span>
                   {item.special && (
-                    <span className="absolute -bottom-1 text-[10px] font-semibold text-purple-700 dark:text-yellow-400 uppercase">
+                    <span className="absolute -bottom-1 text-[10px] font-semibold text-purple-700 dark:text-yellow-600 uppercase">
                       New
                     </span>
                   )}
