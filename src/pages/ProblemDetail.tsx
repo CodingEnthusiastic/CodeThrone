8986f2c1e8158ffcfb916b735262a87e0c0b34bb
Rishabh Shenoy <162883902+CodingEnthusiastic@users.noreply.github.com>
2025-07-31 15:22:06 +0530

    Update ProblemDetail.tsx

--- src/pages/ProblemDetail.tsx
+++ src/pages/ProblemDetail.tsx
@@ -2107,7 +2107,7 @@ const ProblemDetail: React.FC = () => {
                   className="settings-button p-2 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                   title="Editor Settings"
                 >
-                  <Settings className="h-4 w-4" />
+                  <Settings className="h-4 w-4" /> Settings
                 </button>
 
                 {/* Action Buttons */}
